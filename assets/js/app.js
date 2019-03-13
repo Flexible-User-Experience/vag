4d442f4f38f2c282316717963eaf8ff7a39d13f3
David Romaní <david@flux.cat>
2019-03-13 19:44:16 +0100

    improve frontend homepage 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,12 +6,12 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/global.scss');
+require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
 
-require('bootstrap-sass');
+require('bootstrap');
 
 $(document).ready(function() {
     console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
