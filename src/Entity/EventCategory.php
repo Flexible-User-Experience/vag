ec32acc0ace949fd007b0291adf528e5466c4161
David Romaní <david@flux.cat>
2019-03-14 20:08:03 +0100

    add EventCategory

---
+++ src/Entity/EventCategory.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EventCategoryRepository")
+ */
+class EventCategory
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $priority;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isAvailable;
+
+    /**
+     * Methods.
+     */
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return EventCategory
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPriority(): ?int
+    {
+        return $this->priority;
+    }
+
+    /**
+     * @param int $priority
+     *
+     * @return EventCategory
+     */
+    public function setPriority(int $priority): self
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getIsAvailable(): ?bool
+    {
+        return $this->isAvailable;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function isAvailable(): ?bool
+    {
+        return $this->getIsAvailable();
+    }
+
+    /**
+     * @param bool $isAvailable
+     *
+     * @return EventCategory
+     */
+    public function setIsAvailable(bool $isAvailable): self
+    {
+        $this->isAvailable = $isAvailable;
+
+        return $this;
+    }
+}
