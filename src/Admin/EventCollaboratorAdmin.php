f4078adc7ed6ea52bceca18b1c1b03f33218e82b
David Romaní <david@flux.cat>
2019-03-15 20:56:18 +0100

    add EventCollaboratorAdmin

---
+++ src/Admin/EventCollaboratorAdmin.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+/**
+ * Class EventCollaboratorAdmin
+ */
+final class EventCollaboratorAdmin extends AbstractAdmin
+{
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper) {
+        $formMapper
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'label' => 'admin.label.name',
+                ]
+            )
+            ->add(
+                'surname',
+                TextType::class,
+                [
+                    'label' => 'admin.label.surname',
+                ]
+            )
+            ->add(
+                'email',
+                EmailType::class,
+                [
+                    'label' => 'admin.label.email',
+                ]
+            )
+            ->add(
+                'phone',
+                TextType::class,
+                [
+                    'label' => 'admin.label.phone',
+                ]
+            )
+        ;
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper  $datagridMapper)  {
+        $datagridMapper
+            ->add(
+                'name',
+                null,
+                [
+                    'label' => 'admin.label.name',
+                ]
+            )
+            ->add(
+                'surname',
+                null,
+                [
+                    'label' => 'admin.label.surname',
+                ]
+            )
+            ->add(
+                'email',
+                null,
+                [
+                    'label' => 'admin.label.email',
+                ]
+            )
+            ->add(
+                'phone',
+                null,
+                [
+                    'label' => 'admin.label.phone',
+                ]
+            )
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper) {
+        $listMapper
+            ->add(
+                'name',
+                null,
+                [
+                    'label' => 'admin.label.name',
+                ]
+            )
+            ->add(
+                'surname',
+                null,
+                [
+                    'label' => 'admin.label.surname',
+                ]
+            )
+            ->add(
+                'email',
+                null,
+                [
+                    'label' => 'admin.label.email',
+                ]
+            )
+            ->add(
+                'phone',
+                null,
+                [
+                    'label' => 'admin.label.phone',
+                ]
+            )
+        ;
+    }
+}
