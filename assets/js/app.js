3d8fa67cf773efb49193af6e8fcf2d361692df88
David Romaní <david@flux.cat>
2019-03-17 22:18:12 +0100

    add font awesome webpack

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,6 @@ require('bootstrap');
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
 });
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
