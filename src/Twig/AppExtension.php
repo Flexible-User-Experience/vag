2523a9df90c6ddfc57602083ac9d2789159230c9
David Romaní <david@flux.cat>
2019-04-04 19:46:32 +0200

    add EventCategory color & icon management 4

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -17,7 +17,8 @@ class AppExtension extends AbstractExtension
     public function getFilters()
     {
         return [
-            new TwigFilter('category_icon', [$this, 'drawEventCategoryIcon']),
+            new TwigFilter('icon', [$this, 'drawEventCategoryIcon']),
+            new TwigFilter('icon_colored', [$this, 'drawEventCategoryIconWithColor']),
         ];
     }
 
@@ -27,6 +28,16 @@ class AppExtension extends AbstractExtension
      * @return string
      */
     public function drawEventCategoryIcon(EventCategory $category)
+    {
+        return '<i class="'.$category->getIcon().'"></i>';
+    }
+
+    /**
+     * @param EventCategory $category
+     *
+     * @return string
+     */
+    public function drawEventCategoryIconWithColor(EventCategory $category)
     {
         return '<i class="'.$category->getIcon().'" style="color:'.$category->getColor().'"></i>';
     }
