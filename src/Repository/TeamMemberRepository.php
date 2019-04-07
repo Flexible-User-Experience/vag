a580b0f7b139611159bf1e98dc22ee90d65768b7
David Romaní <david@flux.cat>
2019-04-07 13:15:50 +0200

    add ContactMessage

--- src/Repository/TeamMemberRepository.php
+++ src/Repository/TeamMemberRepository.php
@@ -8,10 +8,10 @@ use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
- * @method EventCollaborator|null find($id, $lockMode = null, $lockVersion = null)
- * @method EventCollaborator|null findOneBy(array $criteria, array $orderBy = null)
- * @method EventCollaborator[]    findAll()
- * @method EventCollaborator[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method TeamMember|null find($id, $lockMode = null, $lockVersion = null)
+ * @method TeamMember|null findOneBy(array $criteria, array $orderBy = null)
+ * @method TeamMember[]    findAll()
+ * @method TeamMember[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class TeamMemberRepository extends ServiceEntityRepository
 {
