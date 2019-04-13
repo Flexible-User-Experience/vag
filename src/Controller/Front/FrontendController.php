7fff95ef31abc3d1e2de8307efe17f90462e5d45
David Romaní <david@flux.cat>
2019-04-13 10:52:26 +0200

    add contact and team frontend controller

--- src/Controller/Front/FrontendController.php
+++ src/Controller/Front/FrontendController.php
@@ -43,32 +43,7 @@ class FrontendController extends AbstractController
      */
     public function homepageTest()
     {
-        if (!$this->get('security.authorization_checker')->isGranted(UserRoleEnum::ROLE_CMS)) {
-
-            return $this->createAccessDeniedException();
-        }
-
-        return $this->render('frontend/homepage_test.html.twig', []);
-    }
-
-    /**
-     * @Route("/blog", name="front_blog")
-     *
-     * @return Response
-     */
-    public function blog()
-    {
-        return $this->render('frontend/blog.html.twig', []);
-    }
-
-    /**
-     * @Route({"ca": "/entrades", "es": "/entradas", "en": "/tickets"}, name="front_tickets")
-     *
-     * @return Response
-     */
-    public function tickets()
-    {
-        return $this->render('frontend/tickets.html.twig', []);
+        return !$this->get('security.authorization_checker')->isGranted(UserRoleEnum::ROLE_CMS) ? $this->createAccessDeniedException() : $this->render('frontend/homepage_test.html.twig', []);
     }
 
     /**
@@ -93,5 +68,45 @@ class FrontendController extends AbstractController
                 'activities' => $this->getDoctrine()->getRepository(EventActivity::class)->findAvailableByCategorySortedByName($category)->getQuery()->getResult(),
             ]
         );
+    }
+
+    /**
+     * @Route("/blog", name="front_blog")
+     *
+     * @return Response
+     */
+    public function blog()
+    {
+        return $this->render('frontend/blog.html.twig', []);
+    }
+
+    /**
+     * @Route({"ca": "/entrades", "es": "/entradas", "en": "/tickets"}, name="front_tickets")
+     *
+     * @return Response
+     */
+    public function tickets()
+    {
+        return $this->render('frontend/tickets.html.twig', []);
+    }
+
+    /**
+     * @Route({"ca": "/contacte", "es": "/contacto", "en": "/contact"}, name="front_contact")
+     *
+     * @return Response
+     */
+    public function contact()
+    {
+        return $this->render('frontend/contact.html.twig', []);
+    }
+
+    /**
+     * @Route({"ca": "/equip", "es": "/equipo", "en": "/team"}, name="front_team")
+     *
+     * @return Response
+     */
+    public function team()
+    {
+        return $this->render('frontend/team.html.twig', []);
     }
 }
