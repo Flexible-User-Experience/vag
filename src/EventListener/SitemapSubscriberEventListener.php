cef416cafb01aabf8188b3b6cec3885a6a814bf2
David Romaní <david@flux.cat>
2019-05-03 19:44:53 +0200

    use presta sitemap listener 1

---
+++ src/EventListener/SitemapSubscriberEventListener.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\EventCategory;
+use App\Repository\BlogPostRepository;
+use App\Repository\EventActivityRepository;
+use App\Repository\EventCategoryRepository;
+use App\Repository\EventCollaboratorRepository;
+use App\Repository\EventLocationRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Presta\SitemapBundle\Event\SitemapPopulateEvent;
+use Presta\SitemapBundle\Service\UrlContainerInterface;
+use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
+
+/**
+ * Class SitemapSubscriberEventListener
+ */
+class SitemapSubscriberEventListener implements EventSubscriberInterface
+{
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
+     * @var EventCategoryRepository
+     */
+    private $eventCategoryRepository;
+
+    /**
+     * @var EventCollaboratorRepository
+     */
+    private $eventCollaboratorRepository;
+
+    /**
+     * @var EventActivityRepository
+     */
+    private $eventActivityRepository;
+
+    /**
+     * @var EventLocationRepository
+     */
+    private $eventLocationRepository;
+
+    /**
+     * @var BlogPostRepository
+     */
+    private $blogPostRepository;
+
+    /**
+     * @var array
+     */
+    private $locales;
+
+    /**
+     * Methods
+     */
+
+    /**
+     * @param UrlGeneratorInterface       $urlGenerator
+     * @param EventCategoryRepository     $eventCategoryRepository
+     * @param EventCollaboratorRepository $eventCollaboratorRepository
+     * @param EventActivityRepository     $eventActivityRepository
+     * @param EventLocationRepository     $eventLocationRepository
+     * @param BlogPostRepository          $blogPostRepository
+     * @param string                      $locales
+     */
+    public function __construct(UrlGeneratorInterface $urlGenerator, EventCategoryRepository $eventCategoryRepository, EventCollaboratorRepository $eventCollaboratorRepository, EventActivityRepository $eventActivityRepository, EventLocationRepository $eventLocationRepository, BlogPostRepository $blogPostRepository, string $locales)
+    {
+        $this->urlGenerator = $urlGenerator;
+        $this->eventCategoryRepository = $eventCategoryRepository;
+        $this->eventCollaboratorRepository = $eventCollaboratorRepository;
+        $this->eventActivityRepository = $eventActivityRepository;
+        $this->eventLocationRepository = $eventLocationRepository;
+        $this->blogPostRepository = $blogPostRepository;
+        $this->locales = explode('|', $locales);
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            SitemapPopulateEvent::ON_SITEMAP_POPULATE => 'populate',
+        ];
+    }
+
+    /**
+     * @param SitemapPopulateEvent $event
+     */
+    public function populate(SitemapPopulateEvent $event): void
+    {
+        $section = $event->getSection();
+        if (is_null($section) || $section == 'default') {
+            /** @var string $locale */
+            foreach ($this->locales as $locale) {
+                $this->registerFrontendStaticUrls($event->getUrlContainer(), $locale);
+                $this->registerFrontendEventCategories($event->getUrlContainer(), $locale);
+            }
+        }
+    }
+
+    /**
+     * @param UrlContainerInterface $urls
+     * @param string                $locale
+     */
+    public function registerFrontendStaticUrls(UrlContainerInterface $urls, string $locale): void
+    {
+        $urls->addUrl(
+            new UrlConcrete(
+                $this->urlGenerator->generate(
+                    'front_homepage',
+                    [
+                        '_locale' => $locale,
+                    ],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                )
+            ),
+            'default'
+        );
+        $urls->addUrl(
+            new UrlConcrete(
+                $this->urlGenerator->generate(
+                    'front_blog',
+                    [
+                        '_locale' => $locale,
+                    ],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                )
+            ),
+            'default'
+        );
+        $urls->addUrl(
+            new UrlConcrete(
+                $this->urlGenerator->generate(
+                    'front_tickets',
+                    [
+                        '_locale' => $locale,
+                    ],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                )
+            ),
+            'default'
+        );
+        $urls->addUrl(
+            new UrlConcrete(
+                $this->urlGenerator->generate(
+                    'front_contact',
+                    [
+                        '_locale' => $locale,
+                    ],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                )
+            ),
+            'default'
+        );
+        $urls->addUrl(
+            new UrlConcrete(
+                $this->urlGenerator->generate(
+                    'front_team',
+                    [
+                        '_locale' => $locale,
+                    ],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                )
+            ),
+            'default'
+        );
+    }
+
+    /**
+     * @param UrlContainerInterface $urls
+     * @param string                $locale
+     */
+    public function registerFrontendEventCategories(UrlContainerInterface $urls, string $locale): void
+    {
+        $categories = $this->eventCategoryRepository->findAvailableSortedByPriorityAndName()->getQuery()->getResult();
+
+        /** @var EventCategory $category */
+        foreach ($categories as $category) {
+            $urls->addUrl(
+                new UrlConcrete(
+                    $this->urlGenerator->generate(
+                        'front_event_category',
+                        [
+                            'slug' => $category->getSlug(),
+                            '_locale' => $locale,
+                        ],
+                        UrlGeneratorInterface::ABSOLUTE_URL
+                    )
+                ),
+                'default'
+            );
+        }
+    }
+}
