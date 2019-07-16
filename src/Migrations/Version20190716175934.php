33d9ddc70aefaf66f00e00bdf7ac96dc3b6c0428
David Romaní <david@flux.cat>
2019-07-16 20:05:23 +0200

    add new link name attr in admin

---
+++ src/Migrations/Version20190716175934.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190716175934 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE team_partner ADD link_name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE team_member ADD link_name VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE team_member DROP link_name');
+        $this->addSql('ALTER TABLE team_partner DROP link_name');
+    }
+}
