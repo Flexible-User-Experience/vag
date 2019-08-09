841ae6ca60f96b3f5163545ad5d1953fb4d059b5
David Romaní <david@flux.cat>
2019-08-09 19:12:12 +0200

    fix blog problem & composer update

--- src/Controller/Front/FrontendMainController.php
+++ src/Controller/Front/FrontendMainController.php
@@ -18,7 +18,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @Route("/", requirements={"_locale"="%app_locales%"})
